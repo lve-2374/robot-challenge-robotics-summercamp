0d4b2f88dd8b80fcf3f4a56a6ebddb5073b16574
lve-2374 <schattenfalten@gmail.com>
2025-09-04 14:50:21 +0800

    Add files via upload

---
+++ ros2_ws/install/path_planner_server/share/path_planner_server/launch/pathplanner_real.launch.py
@@ -0,0 +1,59 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    controller_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'controller.yaml')
+    bt_navigator_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'bt_navigator.yaml')
+    planner_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'planner_server.yaml')
+    recovery_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'recovery.yaml')
+
+    use_sim = False
+    
+    return LaunchDescription([     
+        Node(
+            package='nav2_controller',
+            executable='controller_server',
+            name='controller_server',
+            output='screen',
+            parameters=[controller_yaml,
+                        {'use_sim_time': use_sim}]),
+
+        Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            output='screen',
+            parameters=[planner_yaml,
+                        {'use_sim_time': use_sim}]),
+            
+        Node(
+            package='nav2_behaviors',
+            executable='behavior_server',
+            name='recoveries_server',
+            parameters=[recovery_yaml,
+                        {'use_sim_time': use_sim}],
+            output='screen'),
+
+        Node(
+            package='nav2_bt_navigator',
+            executable='bt_navigator',
+            name='bt_navigator',
+            output='screen',
+            parameters=[bt_navigator_yaml,
+                        {'use_sim_time': use_sim}]),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_pathplanner',
+            output='screen',
+            parameters=[{'autostart': True},
+                        {'use_sim_time': use_sim},
+                        {'node_names': ['planner_server',
+                                        'controller_server',
+                                        'recoveries_server',
+                                        'bt_navigator']}])
+    ])
